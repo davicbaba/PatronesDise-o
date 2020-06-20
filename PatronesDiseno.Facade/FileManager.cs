5dcd61557245fb3af6e74d4fb9383161021ea552
Davis Cruz <daviccc98@gmail.com>
2020-06-19 22:47:29 -0600

    Ejemplo de Facade

---
+++ PatronesDiseno.Facade/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PatronesDiseno.Facade
+{
+    public class FileManager
+    {
+        public int CountCharacters(string fileName)
+        {
+            string fullFilePath = GetAbsolutePath(fileName);
+
+            if (File.Exists(fullFilePath) == false)
+                throw new FileNotFoundException("El archivo que especificaste no existe");
+
+            int characterCount = 0;
+
+            string[] lines = File.ReadAllLines(fullFilePath);
+
+            foreach (string line in lines)
+            {
+                characterCount += line.Length;
+            }
+
+            return characterCount;
+        }
+
+        /// <summary>
+        /// Retorna la ruta absoluta de donde se encuentra el archivo.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private string GetAbsolutePath(string fileName)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            return string.Format("{0}Resources\\{1}", path, fileName);
+
+        }
+
+    }
+}
---
+++ PatronesDiseno.Facade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PatronesDiseno.Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager fileManager = new FileManager();
+
+            int count = fileManager.CountCharacters("text.txt");
+
+            Console.WriteLine("La cantidad de caracteres en el archivo son: " + count);
+
+            Console.ReadLine();
+        }
+    }
+}
