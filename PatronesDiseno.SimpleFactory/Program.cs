7ef35ea714cebe4884d1a522b0d14fb0ec0d30ed
Davis Cruz <daviccc98@gmail.com>
2020-07-07 21:29:35 -0600

    Ejemplo Simple Factory

---
+++ PatronesDiseno.SimpleFactory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PatronesDiseno.SimpleFactory
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Este se considera un patron de diseño, el patron real es Factorymethod pero se utiliza bastante el SimpleFactory tambien.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            IGame game = SimpleFactory.CreateGame("chess");
+            IGame game2 = SimpleFactory.CreateGame("solitaire");
+
+            Console.WriteLine(game.GetType().Name);
+            Console.WriteLine(game2.GetType().Name);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PatronesDiseno.SimpleFactory/SimpleFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatronesDiseno.SimpleFactory
+{
+    public class SimpleFactory
+    {
+        public static IGame CreateGame(string type)
+        {
+            if (type == null)
+                return null;
+
+            IGame game = null;
+
+            switch (type)
+            {
+                case "chess":
+                    game = new ChessGame();
+                    break;
+                case "solitaire":
+                    game = new SolitaireGame();
+                    break;
+            }
+
+            return game;
+        }
+
+    }
+}
